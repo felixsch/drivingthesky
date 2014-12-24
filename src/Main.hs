7c94832a98c91471c10bd46ebbef6765308e5fee
Felix Schnizlein <felix@fix3d.net>
2014-12-24 16:11:00 +0100

    The cube....

--- src/Main.hs
+++ src/Main.hs
@@ -28,19 +28,29 @@ render :: GLFW.Window -> GameStatus -> GameState -> Resources -> IO Bool
 render win (GameMainMenu) st mgr = do
 
     clear [ColorBuffer, DepthBuffer]
-    c4 (Color4 0.5 0.5 0.5 1.0)
+    loadIdentity
 
-    cube 0.5
+    lookAt (Vertex3 0.0 0.0 (5.0)) (Vertex3 0.0 0.0 (-1.0)) (Vector3 0.0 1.0 0.0)
+    c4 (Color4 0.5 0.5 0.5 1.0) 
+
+    preservingMatrix $ do
+      cube 0.5
     
     get errors >>= mapM_ (\e -> putStrLn $ "GL Error: " ++ show e)
     GLFW.swapBuffers win
+    flush
     GLFW.pollEvents
     return False
 
 resize :: GLFW.Window -> Int -> Int -> IO ()
 resize win w h = do
     viewport $= (Position 0 0, Size (fromIntegral w) (fromIntegral h))
+    matrixMode $= Projection
+    loadIdentity
     perspective 45.0 (fromIntegral w/ fromIntegral h) 0.1 100.0
+    matrixMode $= Modelview 0
+    loadIdentity
+    flush
     putStrLn "Resize..."
 
 
@@ -92,7 +102,8 @@ main = do
     unless hasInit $ error "Could not intialize GLFW"
 
     GLFW.setErrorCallback $ Just $ (\_ err -> putStrLn $ "GLFW ERROR: " ++ err)
-    m@(Just window) <- GLFW.createWindow 640 360 "DrivingTheSky!" Nothing Nothing
+    m@(Just window) <- GLFW.createWindow 1377 786 "DrivingTheSky!" Nothing Nothing
+    resize window 1377 786
 
     runGLFW window resize (\st -> render window (gameStatus st) st =<< updateResources res st) drivingthesky 
 
